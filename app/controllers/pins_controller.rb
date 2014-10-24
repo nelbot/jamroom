dcf9949b106a52af3295e29bdf581fb9ea20aeaf
nelbot <nelson.cuadras@gmail.com>
2014-10-23 22:11:18 -0400

    Add pins/users association

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,7 +1,9 @@
 class PinsController < ApplicationController
   respond_to :html, :xml, :json
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
-
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  
   def index
     @pins = Pin.all
     respond_with(@pins)
@@ -12,7 +14,7 @@ class PinsController < ApplicationController
   end
 
   def new
-    @pin = Pin.new
+    @pin = current_user.pins.build
     respond_with(@pin)
   end
 
@@ -20,9 +22,10 @@ class PinsController < ApplicationController
   end
 
   def create
-    @pin = Pin.new(pin_params)
+    @pin = current_user.pins.build(pin_params)
     @pin.save
-    respond_with(@pin)
+    flash.notice = "Pin was successfully created"
+    respond_with(@pin) 
   end
 
   def update
@@ -37,10 +40,16 @@ class PinsController < ApplicationController
 
   private
     def set_pin
-      @pin = Pin.find(params[:id])
+      @pin = Pin.find_by(id: params[:id])
+    end
+
+    def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to_pins_path if @pin.nil?
     end
 
     def pin_params
       params.require(:pin).permit(:description)
     end
 end
+
